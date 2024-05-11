e5bf50f499e8b0319503f9657629546219742b43
manuel-rdz <manuel.alj.rdz@gmail.com>
2024-05-11 16:08:24 -0600

    bot template init
    Initial push for quixo bot template.

---
+++ quixo_bot_template.py
@@ -0,0 +1,34 @@
+class QuixoBot:
+    # symbol sera un numero representando el simbolo con el que me
+    # toca jugar. Puede tener el valor 1 o -1;
+    def __init__(self, symbol):
+        pass
+
+    # board es el estado actual del tablero. Sera una matriz de 5x5 que contiene
+    # los siguientes numeros enteros.
+    #  0 - blank cubit
+    #  1 - X cubit
+    # -1 - O cubit
+    def play_turn(self, board):
+        # Esta funcion debe tomar el tablero actual, simular el movimiento deseado
+        # y regresarlo al evaluador.
+        # return new_board
+        pass
+
+    # Esta funcion sera llamada antes de empezar una nueva partida,
+    # por lo que su proposito es resetear cualquier estado que sea necesario
+    # para empezar desde 0.
+    # Tambien recibe el nuevo simbolo con el que empezara la partida.
+    def reset(self, symbol):
+        pass
+
+# Ejemplo de uso
+
+# Creamos el tablero inicial
+board = [0 * 5 for _ in range(5)]
+# Inicializamos el bot 
+bot = QuixoBot(1)
+
+# Jugamos un turno con este bot y recibimos el nuevo estado del tablero.
+new_board = bot.play_turn(board)
+
